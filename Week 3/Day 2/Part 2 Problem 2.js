abda74dae2fdb8faf1e73fc396d14fd83f061602
Pr3vYCa <wendy.prasna@gmail.com>
2022-09-25 15:55:15 +0700

    Create Part 2 Problem 2.js

---
+++ Week 3/Day 2/Part 2 Problem 2.js
@@ -0,0 +1,30 @@
+function palindrome(input) {
+  let counter = 0
+  let length = input.length
+  let half_length = Math.floor(length / 2)
+  let i = 0
+  console.log(half_length+" half-length")
+  while ( i <= half_length) {
+
+    let length_i = length - i - 1
+    console.log(length_i+" length i")
+
+    if (input.charAt(i) == input.charAt(length_i)) {
+      counter++;
+      i++;
+      console.log(counter+" counter")
+      if (counter == half_length) {
+          return true
+      }
+    }  else {
+      return false
+  }
+
+  }
+}
+
+console.log(palindrome("civic"))       // true
+console.log(palindrome("katak"))       // true
+console.log(palindrome("kasur rusak")) // true
+console.log(palindrome("kupu-kupu"))   // false
+console.log(palindrome("lion")) 
