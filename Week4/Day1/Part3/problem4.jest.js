1d0add88ccad074ae949e35db70189d4c29c322a
Pr3vYCa <107352402+Pr3vYCa@users.noreply.github.com>
2022-09-30 13:19:18 +0700

    meanmedian

---
+++ Week4/Day1/Part3/problem4.jest.js
@@ -0,0 +1,16 @@
+const meanmedian = require("./problem3");
+
+describe("", () => {
+    it("", () => {
+        expect(meanmedian([1, 2, 3, 4])).toBe({ mean: 2.5, median:2.5});
+    })
+    it("", () => {
+        expect(meanmedian([1, 2, 3, 4, 5])).toBe({ mean: 3, median:3});
+    })
+    it("", () => {
+        expect(meanmedian([7, 8, 9, 13, 15])).toBe({ mean: 10.4, median:9});
+    })
+
+}
+)
+
